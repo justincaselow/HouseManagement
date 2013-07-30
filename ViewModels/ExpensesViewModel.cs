7759d73e3fb1976a90f4818e6e5ef4cbed3ef155
justincaselow <justincaselow@gmail.com>
2013-07-30 22:43:01 +0100

    Adding ExpensesViewModel

---
+++ ViewModels/ExpensesViewModel.cs
@@ -0,0 +1,35 @@
+﻿using HouseManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Objects;
+using System.Linq;
+using System.Web;
+
+namespace HouseManagement.ViewModels
+{
+    public class ExpensesViewModel
+    {
+        private HouseModelContainer context = new HouseModelContainer();
+
+        public ObjectSet<ItemType> ItemTypes
+        {
+            get
+            {
+                return context.ItemTypes;
+            }
+        }
+
+        public string Description { get; set; }
+        public decimal GbpAmount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public int Quantity { get; set; }
+
+        public Person Payee
+        {
+            get
+            {
+                return context.People.Single(p => p.Name == "Justin Low");
+            }
+        }
+    }
+}
